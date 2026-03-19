89b8fb8e6e532a1e19aa5b257d59ccd49b209c40
strangerhm3@gmail.com <strangerhm3@gmail.com>
2026-03-19 11:39:33 +0200

    file containing flask api with get and post player record

---
+++ api.py
@@ -0,0 +1,70 @@
+from flask import Flask, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+
+def get_db():
+    return sqlite3.connect('scores.db')
+
+
+@app.route('/add', methods=['POST'])
+def add_score():
+    data = request.json
+    name = data['name']
+    score = data['score']
+
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO scores (name, score) VALUES (?, ?)", (name, score))
+    conn.commit()
+    conn.close()
+
+    return jsonify({"message": "Score added"})
+
+
+@app.route('/score/<name>', methods=['GET'])
+def get_score(name):
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT score FROM scores WHERE name = ?", (name,))
+    result = cursor.fetchall()
+
+    conn.close()
+
+    return jsonify(result)
+
+
+@app.route('/top', methods=['GET'])
+def top_scores():
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT name, score FROM scores")
+    records = cursor.fetchall()
+
+    conn.close()
+
+    max_score = records[0][1]
+
+    for i in range(len(records)):
+        score = records[i][1]
+        if score > max_score:
+            max_score = score
+
+    top_people = []
+
+    for i in range(len(records)):
+        if records[i][1] == max_score:
+            top_people.append(records[i][0])
+
+    return jsonify({
+        "top_scorers": top_people,
+        "score": max_score
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
